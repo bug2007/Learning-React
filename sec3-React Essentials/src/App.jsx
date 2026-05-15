157932af30758867b83b146f9f126c3dbdf8da36
bug2007 <moumitah2007@gmail.com>
2026-05-15 14:00:24 +0600

    props

--- sec3-React Essentials/src/App.jsx
+++ sec3-React Essentials/src/App.jsx
@@ -1,23 +1,72 @@
 // contains the html that we seeing on the screen. jsx = javascript syntax extension. allows to write html in js file.
 // a component is really just a js function that retuns some jsx code.
+
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
   )
 }
+
+// function CoreConcept(props) {  // props is an object that contains all the prop names and their values you passed.
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   )
+// }
+
+// OR
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* prop names are up to you */}
+            {/* <CoreConcept 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} /> */}
+            {/* OR */}
+            <CoreConcept { ...CORE_CONCEPTS[0]} />
+            <CoreConcept { ...CORE_CONCEPTS[1]} />
+            <CoreConcept { ...CORE_CONCEPTS[2]} />
+            <CoreConcept { ...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
--- sec3-React Essentials/src/index.jsx
+++ sec3-React Essentials/src/index.jsx
@@ -4,4 +4,4 @@ import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(<App />);  // injects the App component into the root div in index.html. createRoot() sets the root div as the root of the react project.
