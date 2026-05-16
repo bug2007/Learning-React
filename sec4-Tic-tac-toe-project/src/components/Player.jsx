f8a304f117fe166a82ce03b3f15b261f4ba616b5
bug2007 <moumitah2007@gmail.com>
2026-05-16 13:39:16 +0600

    updating state based on old state

--- sec4-Tic-tac-toe-project/src/components/Player.jsx
+++ sec4-Tic-tac-toe-project/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({ name,symbol }) {
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);  // the input 'editing' is the current state 'isEditing"
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    // let btnCaption = "Edit";
+
+    if (isEditing) {
+        playerName = <input type="text" required value={name} />;
+        // btnCaption = "Save";
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 }
