03c6de9ee5206ca785c41e8427eb56791838e29d
bug2007 <moumitah2007@gmail.com>
2026-05-17 15:02:00 +0600

    results component

--- sec5-Investment-calculator-project/src/App.jsx
+++ sec5-Investment-calculator-project/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [ userInput, setUserInput ] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+  function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue  // newValue is originally a string value that can be converted to numvber value by adding a '+' before it
+            };
+        });
+    }
+
+
   return (
     <>
     <Header />
-    <UserInput />
+    <UserInput userInput={userInput} onChange={handleChange} />
+    <Results input={userInput} />
     </>
   )
 }
