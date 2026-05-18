8145d7cc196546b10133542cc90fff4738072f90
bug2007 <moumitah2007@gmail.com>
2026-05-18 12:43:33 +0600

    used React's StrictMode component

--- sec7-Debugging-React-apps/src/index.jsx
+++ sec7-Debugging-React-apps/src/index.jsx
@@ -1,6 +1,8 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// we cud also import and wrap the StrictMode component around other components, not necessarily around the root component
+ReactDOM.createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>);
