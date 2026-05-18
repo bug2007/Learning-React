fab597dc965d6d3711124621d94559b6616ae9e3
bug2007 <moumitah2007@gmail.com>
2026-05-18 17:21:16 +0600

    used portal to positon JSX code in certain place

--- sec8-Almost-Final-Countdown-Game-project-refs-and-portals/src/components/ResultModal.jsx
+++ sec8-Almost-Final-Countdown-Game-project-refs-and-portals/src/components/ResultModal.jsx
@@ -1,8 +1,13 @@
 import { useImperativeHandle, useRef } from 'react'; // used to expose component APIs
+import { createPortal } from 'react-dom'; // use it to position certain stuff in certain places. for example, to position ResultModal in <div id="modal"> (in the index.html file) even though it's initially placed in TimerChallenge.jsx. Kind of teleporting code
 
-export default function ResultModal({ref, result, targetTime}) {
+export default function ResultModal({ref, targetTime, remainingTime, onReset}) {
 
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime/(targetTime*1000)) * 100);
     
     useImperativeHandle(ref, () => {
         return {
@@ -12,14 +17,16 @@ export default function ResultModal({ref, result, targetTime}) {
         }
     })
 
-    return (
+    return createPortal (
         <dialog ref={dialog} className="result-modal">
-          <h2>{result}</h2>
+          {userLost && <h2>You lost</h2>}
+          {!userLost && <h2>Your Score: {score}</h2>}
           <p>The target time was <strong>{targetTime} seconds.</strong></p>
-          <p>You stopped the timer with <strong>X seconds left.</strong></p>
-          <form method="dialog">
+          <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+          <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
           </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 }
--- sec8-Almost-Final-Countdown-Game-project-refs-and-portals/src/components/TimerChallenge.jsx
+++ sec8-Almost-Final-Countdown-Game-project-refs-and-portals/src/components/TimerChallenge.jsx
@@ -3,36 +3,46 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
-
     const timer = useRef();  // had we used just a normal variable (let timer;), it wud have re-executed with the component and thus been reset. but refs, like states, are not re-executed along with the component.
     const dialog = useRef();
 
+    const [timeRemaining, setTimeRemaining] = useState(targetTime*1000);
+
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+        dialog.current.open();  // dialog.current holds the object that's returned by the useImperativeHandle in ResultModal.jsx
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
+    }
+
     function handleStart() {
-        setTimerStarted(true);
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.open();  // dialog.current holds the object that's returned by the useImperativeHandle in ResultModal.jsx
-        }, targetTime * 1000);
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
     }
 
     function handleStop() {
-        clearTimeout(timer.current);
+        dialog.current.open();  // dialog.current holds the object that's returned by the useImperativeHandle in ResultModal.jsx
+        clearInterval(timer.current);
     }
 
     return (
         <>
-        <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+        {/* ResultModal is set to dislay: none by default in the CSS file */}
+        <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} /> 
         <section className="challenge">
             <h2>{title}</h2>
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
+                <button onClick={timerIsActive ? handleStop : handleStart}>{timerIsActive ? 'Stop' : 'Start'} Challenge</button>
             </p>
-            <p className={timerStarted ? 'active' : undefined}>{timerStarted ? 'Time is running...' : 'Timer inactive'}</p>
+            <p className={timerIsActive ? 'active' : undefined}>{timerIsActive ? 'Time is running...' : 'Timer inactive'}</p>
         </section>
          </>
     )
