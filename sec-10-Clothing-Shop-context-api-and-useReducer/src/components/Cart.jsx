0ceba226c8b2ee70c2e9da39a2b6112fba13a1bd
bug2007 <moumitah2007@gmail.com>
2026-05-19 16:10:05 +0600

    created context, provided context and consumed context

--- sec-10-Clothing-Shop-context-api-and-useReducer/src/components/Cart.jsx
+++ sec-10-Clothing-Shop-context-api-and-useReducer/src/components/Cart.jsx
@@ -1,5 +1,15 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import { useContext } from "react"; // use the 1st one to access some context inside a component but cant use it inside if else blocks
+// import { use } from 'react';    can also use this to access some context inside a component but can use it inside if else blocks
+
+import { CartContext } from "../store/shopping-cart-context.jsx";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  // const { items } = useContext(CartContext);
+  // OR 
+
+  const cartCtx = useContext(CartContext); // returns the value that is provided by the CartContext
+
+  const totalPrice = cartCtx.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -7,10 +17,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
