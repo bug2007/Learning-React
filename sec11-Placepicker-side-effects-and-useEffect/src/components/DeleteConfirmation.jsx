684840529ab816cb5a31bcf4b9481e94b2f79941
bug2007 <moumitah2007@gmail.com>
2026-05-20 13:16:40 +0600

    added useEffect's cleanup function

--- sec11-Placepicker-side-effects-and-useEffect/src/components/DeleteConfirmation.jsx
+++ sec11-Placepicker-side-effects-and-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  
+  useEffect(() => {  // modal opens. if we didnt close the delete confirmation modal even after 3s, the place shud be automatically deleted
+    const timer = setTimeout(() => {
+      onConfirm()}, 3000);
+
+      return () => { clearTimeout(timer) }   // cleanup func that will be executed right before the effect func runs again or right before this component dismounts. but it does not execute right before the effect func runs for the first time
+  }, [])
+  
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
