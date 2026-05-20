514d80e8b97c6a13fd0cf525769c5d9d45116cfc
bug2007 <moumitah2007@gmail.com>
2026-05-20 12:41:04 +0600

    understood effect dependencies

--- sec11-Placepicker-side-effects-and-useEffect/src/components/Modal.jsx
+++ sec11-Placepicker-side-effects-and-useEffect/src/components/Modal.jsx
@@ -11,7 +11,7 @@ function Modal({ open, children }) {  // open is either true or false
     } else {
       dialog.current.close();
     }
-  }, []);
+  }, [open]); // effect dependencies are any value that causes the component to execute again. this effect func shud run whenever the component func executed if one of the dependencies changed
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
