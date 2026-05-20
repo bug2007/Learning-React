8edba8fc8c4e5f742e17d05e9e13778c86c6a594
bug2007 <moumitah2007@gmail.com>
2026-05-21 01:01:07 +0600

    outputted quiz results

--- sec12-Quiz-App/src/components/Question.jsx
+++ sec12-Quiz-App/src/components/Question.jsx
@@ -1,13 +1,56 @@
+import { useState } from "react";
+
 import QuestionTimer from "./QuestionTimer.jsx"
 import Answers from "./Answers.jsx";
+import QUESTIONS from '../questions.js';
 
-export default function Question({onSkipAnswer, questionText, answers, onSelectAnswer, selectedAnswer, answerState}) {
+export default function Question({index, onSkipAnswer, onSelectAnswer}) {  // cant use key prop by ourselves. shud only be used by React
+    const [answer, setAnswer] = useState({
+        selectedAnswer: '',
+        isCorrect: null
+    })
+
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
+
+    function handleSelectAnswer(answer) {
+        setAnswer({
+            selectedAnswer: answer,
+            isCorrect: null
+        })
+
+        setTimeout(() => {
+            setAnswer({
+                selectedAnswer: answer,
+                isCorrect: QUESTIONS[index].answers[0] === answer
+            })
+
+            setTimeout(() => {
+                onSelectAnswer(answer);
+            })
+        }, 1000)
+    }
+
+    let answerState = '';
+
+    if (answer.selectedAnswer && answer.isCorrect != null) {
+        answerState = answer.isCorrect ? 'correct' : 'wrong';
+    } else if (answer.selectedAnswer) {
+        answerState = 'answered'
+    }
+    
     return (
         <div id="question">
-            {/* setting this unique key (a built-in prop) so that the QuestionTimer is unmounted and remounted from the DOM everytime and as a result, those timer and interval in the QuestionTimer component will cleanup and reexecute the effect funcs, thus setting a new timer and new interval everytime. that way every new question will have a new timer and new interval set again  */}
-            <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
-            <h2>{questionText}</h2>
-            <Answers answers={answers} selectedAnswer={selectedAnswer} answerState={answerState} onSelect={onSelectAnswer} />
+            <QuestionTimer key={timer} timeout={timer} onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null} mode={answerState} />
+            <h2>{QUESTIONS[index].text}</h2>
+            <Answers answers={QUESTIONS[index].answers} selectedAnswer={answer.selectedAnswer} answerState={answerState} onSelect={handleSelectAnswer} />
         </div>
     )
 }
--- sec12-Quiz-App/src/components/Quiz.jsx
+++ sec12-Quiz-App/src/components/Quiz.jsx
@@ -2,48 +2,32 @@ import { useState, useCallback } from "react";
 
 import QUESTIONS from '../questions.js';
 import Question from "./Question.jsx";
-import quizCompleteImg from '../assets/quiz-complete.png';
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-        setAnswerState('answered');
         setUserAnswers(prevUserAnswers => {
             return [...prevUserAnswers, selectedAnswer]
         })
-
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState('correct')
-            } else {
-                setAnswerState('wrong')
-            }
-
-            setTimeout(() => {
-                setAnswerState('')
-            }, 2000)
-        }, 1000)
-    }, [activeQuestionIndex])
+    }, [])
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
         return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy icon" />
-                <h2>Quiz Completed!</h2>
-            </div>
+            <Summary userAnswers={userAnswers} />
         )
     }
 
     return (
         <div id="quiz">
-            <Question key={activeQuestionIndex} onSkipAnswer={handleSkipAnswer} answerState={answerState} selectedAnswer={userAnswers[userAnswers.length-1]} questionText={QUESTIONS[activeQuestionIndex].text} answers={QUESTIONS[activeQuestionIndex].answers} onSelectAnswer={handleSelectAnswer} />
+            {/* setting unique key (a built-in prop) so that the Question is unmounted and remounted from the DOM everytime and as a result, those timer and interval in the QuestionTimer component will cleanup and reexecute the effect funcs, thus setting a new timer and new interval everytime. that way every new question will have a new timer and new interval set again  */}
+            <Question key={activeQuestionIndex} index={activeQuestionIndex} onSkipAnswer={handleSkipAnswer} onSelectAnswer={handleSelectAnswer} />
         </div>
     )
 }
