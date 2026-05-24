1aa650c073d1c40f16a8965225dcdc7624e0d75e
bug2007 <moumitah2007@gmail.com>
2026-05-25 00:18:59 +0600

    handled form submission

--- sec18-React-Form-form-actions/src/components/Signup.jsx
+++ sec18-React-Form-form-actions/src/components/Signup.jsx
@@ -2,8 +2,7 @@ import { isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength } from '../util/
 
 import { useActionState } from 'react';
 
-export default function Signup() {
-  function signupAction(prevFormState, formData) {  // when this func is executed for the first time, prevFormState will simply be the initial {errors: null} 
+function signupAction(prevFormState, formData) {  // when this func is executed for the first time, prevFormState will simply be the initial {errors: null} 
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -44,12 +43,13 @@ export default function Signup() {
     }
 
     if (errors.length > 0) {
-      return { errors };   // same as { errors: errors}
+      return { errors, enteredValues: {email, password, confirmPassword, firstName, lastName, role, acquisitionChannel, terms} };   // same as { errors: errors}
     }
 
     return { errors: null }
   }
 
+export default function Signup() {
   const [formState, formAction] = useActionState(signupAction, {errors: null}) // 1st parameter: action func, 2nd parameter: initial state value if the form hasnt been submitted yet. form state: initial state or the returned state if func has been executed. formAction: an updated sigupAction() func. pending: a boolean depending on whether the form is currently being submitted or not
   // const [formState, formAction, pending] = useActionState(signupAction, {errors: null}) // 1st parameter: action func, 2nd parameter: initial state value if the form hasnt been submitted yet. form state: initial state or the returned state if func has been executed. formAction: an updated sigupAction() func. pending: a boolean depending on whether the form is currently being submitted or not
 
@@ -61,13 +61,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" defaultValue={formState.enteredValues?.password} />
         </div>
 
         <div className="control">
@@ -76,6 +76,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -85,18 +86,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName} />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName} />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" defaultValue={formState.enteredValues?.role}>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -113,6 +114,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -123,19 +125,20 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other" defaultChecked={formState.enteredValues?.acquisitionChannel.includes('other')}/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" defaultChecked={formState.enteredValues?.terms}/>I
           agree to the terms and conditions
         </label>
       </div>
