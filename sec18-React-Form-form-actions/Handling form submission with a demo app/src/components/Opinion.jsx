28bd0b3d9ff082a4f2a24c0dbf2305b5d614e371
bug2007 <moumitah2007@gmail.com>
2026-05-26 00:46:08 +0600

    added header component to Food App

--- sec18-React-Form-form-actions/Handling form submission with a demo app/src/components/Opinion.jsx
+++ sec18-React-Form-form-actions/Handling form submission with a demo app/src/components/Opinion.jsx
@@ -1,16 +1,24 @@
-import { use, useActionState } from 'react';
+import { use, useActionState, useOptimistic } from 'react';  // useOptimistic helps us with optimistic updating. it is meant to be used in conjunction with form actions
 
 import { OpinionsContext } from '../store/opinions-context';
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext)
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(votes, (prevVotes, mode) =>   // votes is a state that is returned in optimisticVotes. setVotesOptimistically is a func u can call which is defined inside the useOptimistic(): the 2nd parameter. the func shud be called inside of a form action. optimisticVotes is a temporary state thats only shown on the UI whilst the form (that invoked this optimistic func) is being submitted. then it will be thrown away and the UI will be updated with the value its supposed to after the form submission is complete. component will re-execute just as it does with useState. if there's suppose a backend error, then the value will be updated temporarily with the new temporary optimisticVotes state before rolling back to the old value  
+    mode === 'up' ? prevVotes + 1 : prevVotes - 1
+  )
+
   async function upvoteAction() {
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
+    // once the form action completes, useOptimistic will get rid of the optimisticVotes state and instead apply the regular UI state
   }
 
   async function downvoteAction() {
+    setVotesOptimistically('down');
     await downvoteOpinion(id);
+    // once the form action completes, useOptimistic will get rid of the optimisticVotes state and instead apply the regular UI state
   }
 
   const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction);
@@ -42,7 +50,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button formAction={downvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
