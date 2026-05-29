59ac21ab614b4c7dc06532ff1abcd30eb0454efa
bug2007 <moumitah2007@gmail.com>
2026-05-29 23:23:12 +0600

    worked with multiple state properties

--- sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/components/Counter.js
+++ sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch(); // dispatch here is a func which will dispatch an action against our redux store
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({type: 'increment'})
@@ -17,12 +18,14 @@ const Counter = () => {
     dispatch({type: 'increase', amount: 5})
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
--- sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
+++ sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
@@ -1,21 +1,33 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) => {  // will be called when action is dispatched or when the store is created the 1st time
+const initialState = {counter: 0, showCounter: true};
+
+const counterReducer = (state = initialState, action) => {  // will be called when action is dispatched or when the store is created the 1st time
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter  // gotta return everything even if showCounter value doesnt change
         }
     }
 
     if (action.type === 'increase') {
         return {
-            counter: state.counter + action.amount
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
         }
     }
 
