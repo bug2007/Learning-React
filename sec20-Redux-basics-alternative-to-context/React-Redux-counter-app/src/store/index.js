cc3d0e4a7f23cd94611f064752087abd2ad3bcba
bug2007 <moumitah2007@gmail.com>
2026-05-29 23:10:59 +0600

    attached payload to action

--- sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
+++ sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
@@ -7,6 +7,12 @@ const counterReducer = (state = {counter: 0}, action) => {  // will be called wh
         }
     }
 
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount
+        }
+    }
+
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1
