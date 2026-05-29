0e35222c015b9759a105dd865b1d5313d792a923
bug2007 <moumitah2007@gmail.com>
2026-05-30 00:03:12 +0600

    added state slices

--- sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
+++ sec20-Redux-basics-alternative-to-context/React-Redux-counter-app/src/store/index.js
@@ -1,6 +1,26 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {counter: 0, showCounter: true};
+
+createSlice({
+    name: 'counter',  // always needs a name
+    initialState, // same as initialState: initialState
+    reducers: {
+        increment(state) { // receives latest state
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {  // can receive action if we want it to
+            state.counter = state.counter + action.amount;
+        }, 
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+})
 
 const counterReducer = (state = initialState, action) => {  // will be called when action is dispatched or when the store is created the 1st time
     if (action.type === 'increment') {
