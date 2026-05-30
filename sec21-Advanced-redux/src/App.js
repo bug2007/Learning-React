de0c2872ea3de58b60d5ab67841efd1b7e27b700
bug2007 <moumitah2007@gmail.com>
2026-05-31 00:38:10 +0600

    handled http states & feedback with redux

--- sec21-Advanced-redux/src/App.js
+++ sec21-Advanced-redux/src/App.js
@@ -1,25 +1,64 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch('https://react-redux-app-4eee7-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',      // will create a new cart Node in database (firebase) and store the data there. PUT will not add the new data in a list of data unlike POST, instead will override the existing cart
-      {method: 'PUT',
-        body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }))
+      const response = await fetch('https://react-redux-app-4eee7-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',      // will create a new cart Node in database (firebase) and store the data there. PUT will not add the new data in a list of data unlike POST, instead will override the existing cart
+        {method: 'PUT',
+          body: JSON.stringify(cart)
       }) 
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!'
+      }))
+    }
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!'
+      }))
+    })
   }, [cart])   // so that this effect func reexecutes whenever the cart in our Redux store changes
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- sec21-Advanced-redux/src/store/ui-slice.js
+++ sec21-Advanced-redux/src/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartIsVisible: false},
+    initialState: { cartIsVisible: false, notification: null},
     reducers: {     
         toggle(state) {      // DO NOT write any async code (e.g sending HTTP req) or side effect code inside of any reducer func. 
             state.cartIsVisible = !state.cartIsVisible;
+        },
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
         }
     }
 })
