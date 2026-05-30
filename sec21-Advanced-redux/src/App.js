f5d77b607645e1c913e98bf86ea9c642dec67244
bug2007 <moumitah2007@gmail.com>
2026-05-30 23:59:46 +0600

    used useEffect with redux

--- sec21-Advanced-redux/src/App.js
+++ sec21-Advanced-redux/src/App.js
@@ -2,9 +2,18 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://react-redux-app-4eee7-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',      // will create a new cart Node in database (firebase) and store the data there. PUT will not add the new data in a list of data unlike POST, instead will override the existing cart
+      {method: 'PUT',
+        body: JSON.stringify(cart)
+      }) 
+  }, [cart])   // so that this effect func reexecutes whenever the cart in our Redux store changes
 
   return (
     <Layout>
--- sec21-Advanced-redux/src/store/ui-slice.js
+++ sec21-Advanced-redux/src/store/ui-slice.js
@@ -3,12 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const uiSlice = createSlice({
     name: 'ui',
     initialState: { cartIsVisible: false},
-    reducers: {
-        toggle(state) {
+    reducers: {     
+        toggle(state) {      // DO NOT write any async code (e.g sending HTTP req) or side effect code inside of any reducer func. 
             state.cartIsVisible = !state.cartIsVisible;
         }
     }
 })
+
 
 export const uiActions = uiSlice.actions;
 
