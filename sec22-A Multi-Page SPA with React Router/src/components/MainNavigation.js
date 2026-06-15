0f19c68eed0ce7f2eba962a50d437442255d72a1
bug2007 <moumitah2007@gmail.com>
2026-06-15 12:59:27 +0600

    integrated NavLink

--- sec22-A Multi-Page SPA with React Router/src/components/MainNavigation.js
+++ sec22-A Multi-Page SPA with React Router/src/components/MainNavigation.js
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';  // wont send a new HTTP req and load all the js code again from scratch. this way, state is also preserved.
+
+import { NavLink } from 'react-router-dom';  // just like Link but with it, we can use the isActive to check if the link is currently active and led to its route
+
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -6,8 +9,10 @@ function MainNavigation() {
         <header className={classes.header}>
             <nav>
                 <ul className={classes.list}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/products'>Products</Link></li>
+                    <li><NavLink to='/' className={({isActive}) => isActive ? classes.active : undefined} end
+                    // style={({isActive}) => ({textAlign: isActive ? 'center' : 'left'})}
+                    >Home</NavLink></li>
+                    <li><NavLink to='/products' className={({isActive}) => isActive ? classes.active : undefined}>Products</NavLink></li>
                 </ul>
             </nav>
         </header>
