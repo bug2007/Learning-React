bd41ffee17a69912fa7a68c83a652510f263e51c
bug2007 <moumitah2007@gmail.com>
2026-06-15 14:10:07 +0600

    index routes

--- sec22-A Multi-Page SPA with React Router/src/App.js
+++ sec22-A Multi-Page SPA with React Router/src/App.js
@@ -11,7 +11,10 @@ const router = createBrowserRouter([
     element: <RootLayout />, //root layout wud be in every page u go to. it acts as a wrapper for the children components below. RootLayout will be in the HomePage, ProductsPage etc.
     errorElement: <ErrorPage />,
     children: [
-      {path: '/', element: <HomePage />},  
+      // {path: '', element: <HomePage />},
+      // {path: '/', element: <HomePage />},   // absolute paths if they start with slash. relative paths if they dont. relative paths are appended to the current url 
+      {index: true, element: <HomePage />}, // the default component that shud be displayed if the parent's route is active
+      // {path: 'products', element: <ProductsPage />},
       {path: '/products', element: <ProductsPage />},
       {path: '/products/:productId', element: <ProductDetailPage />}  // u can extract this dynamic productId value inside ProductDetailPage component using useParams
     ]
