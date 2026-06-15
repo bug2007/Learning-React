a5d4ab8223cc7654e10c8da42689a2a143c08c22
bug2007 <moumitah2007@gmail.com>
2026-06-15 13:37:04 +0600

    dynamic routes

--- sec22-A Multi-Page SPA with React Router/src/App.js
+++ sec22-A Multi-Page SPA with React Router/src/App.js
@@ -3,15 +3,17 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 const router = createBrowserRouter([
   {
     path: '/', 
-    element: <RootLayout />, //root layout wud be in every page u go to. it acts as a wrapper for the children components below. RootLayout will be in both the HomePage and ProductsPage
+    element: <RootLayout />, //root layout wud be in every page u go to. it acts as a wrapper for the children components below. RootLayout will be in the HomePage, ProductsPage etc.
     errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <HomePage />},  
-      {path: '/products', element: <ProductsPage />}
+      {path: '/products', element: <ProductsPage />},
+      {path: '/products/:productId', element: <ProductDetailPage />}  // u can extract this dynamic productId value inside ProductDetailPage component using useParams
     ]
   },
   
--- sec22-A Multi-Page SPA with React Router/src/pages/Home.js
+++ sec22-A Multi-Page SPA with React Router/src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom'; // wont send a new HTTP req and load all the js code again from scratch. this way, state is also preserved.
+import { Link, useNavigate } from 'react-router-dom'; // using Link wont send a new HTTP req and load all the js code again from scratch. this way, state is also preserved.
 
 function HomePage() {
+    const navigate = useNavigate(); // a navigate func to navigate programmatically
+
+    function navigateHandler() {
+        navigate('/products');
+    }
+
     return (
         <>
         <h1>My Home Page</h1>
         {/* <p>Go to <a href="/products">the list of products</a>.</p> */}
         <p>Go to <Link to="/products">the list of products</Link>.</p>
+        <p>
+            <button onClick={navigateHandler}>Navigate</button>
+        </p>
         </>
     ) 
 }
