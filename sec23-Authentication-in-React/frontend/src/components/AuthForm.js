ae3642c659a3a7fac5e5dc623ef4b316c2481426
bug2007 <moumitah2007@gmail.com>
2026-06-16 16:15:39 +0600

    query parameters, auth action

---
+++ sec23-Authentication-in-React/frontend/src/components/AuthForm.js
@@ -0,0 +1,43 @@
+// import { useState } from 'react';
+import { Form, Link, useSearchParams } from 'react-router-dom'; // useSearchParams to access query parameters. remember query parameters are in the form '.....?mode=signup' whereas normal parameters are in the from ':eventId/edit'
+
+import classes from './AuthForm.module.css';
+
+function AuthForm() {
+  // const [isLogin, setIsLogin] = useState(true);
+
+  // function switchAuthHandler() {
+  //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  // }
+
+  // const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get('mode') === 'login';
+
+  return (
+    <>
+      <Form method="post" className={classes.form}>
+        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="image">Password</label>
+          <input id="password" type="password" name="password" required />
+        </p>
+        <div className={classes.actions}>
+          {/* <button onClick={switchAuthHandler} type="button">
+            {isLogin ? 'Create new user' : 'Login'}
+          </button> */}
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
+          <button>Save</button>
+        </div>
+      </Form>
+    </>
+  );
+}
+
+export default AuthForm;
