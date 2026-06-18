1d370e38a3f4c60190a7e8453705c633017cb2bc
bug2007 <moumitah2007@gmail.com>
2026-06-18 14:30:58 +0600

    acted on mutation success and invalidate queries

--- sec25-React Query or Tanstack Query/src/components/Events/NewEvent.jsx
+++ sec25-React Query or Tanstack Query/src/components/Events/NewEvent.jsx
@@ -5,12 +5,17 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/http.js'
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: createNewEvent // useMutation will not send this req right away (unlike useQuery). instead u can the func 'mutate' to send the req whenever u want
+    mutationFn: createNewEvent, // useMutation will not send this req right away (unlike useQuery). instead u can the func 'mutate' to send the req whenever u want
+    onSuccess: () => {  // a func that will be executed once the mutation succeeds. will execute only if the mutation succeeds
+      queryClient.invalidateQueries({queryKey: ['events']}); // tells the react query that the data fetched by certain queries is outdated now (since we added a new event by using useMutation) that it should be marked as stale and an immediate refetch shud be triggered if the query belongs to a component that's currently visible on the screen (because background refetch happens when u go away and come back. it wont happen if the component e.g the events component, is still visible on the screen). all the queries that involve the queryKey 'events' (even the one in FindEventSection.jsx if u didnt say exact: true) will be invalidated
+      navigate('/events')   
+    }
   })
 
   function handleSubmit(formData) {
