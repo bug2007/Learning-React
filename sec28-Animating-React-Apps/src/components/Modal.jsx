3e34a12c75dab44362aa85f8929b2ba1dc47ddbf
bug2007 <moumitah2007@gmail.com>
2026-06-21 10:07:31 +0600

    made elements pop with hover animations

--- sec28-Animating-React-Apps/src/components/Modal.jsx
+++ sec28-Animating-React-Apps/src/components/Modal.jsx
@@ -1,14 +1,19 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog 
+        initial={{opacity: 0, y: 30}}
+        animate={{opacity: 1, y: 0}}
+        exit={{opacity: 0, y: 30}}  // import AnimatePresence in Header.jsx for it to work. because by default, when react removes elements from the DOM, they instantly disappear without any animation
+        open className="modal">
         <h2>{title}</h2>
         {children}
-      </dialog>
-    </>,
+      </motion.dialog>
+    </>, 
     document.getElementById('modal')
   );
 }
