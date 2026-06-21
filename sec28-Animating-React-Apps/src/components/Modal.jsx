03b9b974d620ec75006dfa0aa19da47d4a82eec4
bug2007 <moumitah2007@gmail.com>
2026-06-21 10:12:35 +0600

    reused animation states with variants

--- sec28-Animating-React-Apps/src/components/Modal.jsx
+++ sec28-Animating-React-Apps/src/components/Modal.jsx
@@ -6,9 +6,16 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog 
-        initial={{opacity: 0, y: 30}}
-        animate={{opacity: 1, y: 0}}
-        exit={{opacity: 0, y: 30}}  // import AnimatePresence in Header.jsx for it to work. because by default, when react removes elements from the DOM, they instantly disappear without any animation
+        // initial={{opacity: 0, y: 30}}
+        // animate={{opacity: 1, y: 0}}
+        // exit={{opacity: 0, y: 30}}  // import AnimatePresence in Header.jsx for it to work. because by default, when react removes elements from the DOM, they instantly disappear without any animation
+        variants={{
+          hidden: {opacity: 0, y: 30},  // key names are upto u
+          visible: {opacity: 1, y: 0}
+        }}
+        initial='hidden'
+        animate='visible'
+        exit='hidden'
         open className="modal">
         <h2>{title}</h2>
         {children}
