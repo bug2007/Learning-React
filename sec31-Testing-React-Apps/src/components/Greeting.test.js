fccb30c5df63324afa6c7b84ffa82c423804a9ff
bug2007 <moumitah2007@gmail.com>
2026-06-22 17:01:46 +0600

    grouped tests into test suite

--- sec31-Testing-React-Apps/src/components/Greeting.test.js
+++ sec31-Testing-React-Apps/src/components/Greeting.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test('renders Hello World as a text', () => {
-    // Arrange
-    render(<Greeting />)
-
-    // Act
-    // ...nothing
-
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World', {exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
+// we can group tests into test suites
+describe('Greeting component', () => {  // 1st parameter: desc. of the category to which the tests belong. 2nd parameter: the actual tests. can have multiple tests per test suite
+    test('renders Hello World as a text', () => {
+        // Arrange
+        render(<Greeting />)
+    
+        // Act
+        // ...nothing
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World', {exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    })
 })
