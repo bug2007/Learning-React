0cb8b1680d72c81d7aca746b4128595f2fb177aa
bug2007 <moumitah2007@gmail.com>
2026-06-22 13:23:43 +0600

    worked with debouncing

--- sec29-React-patterns-and-best-practices/src/App.jsx
+++ sec29-React-patterns-and-best-practices/src/App.jsx
@@ -35,11 +35,11 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES}>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
           {/* can return whichever JSX code we want */}
           {(item) => <Place item={item} />}  
         </SearchableList>
-        <SearchableList items={['item1', 'item2']}>
+        <SearchableList items={['item1', 'item2']} itemKeyFn={(item) => item}>
           {(item) => item}
         </SearchableList>
       </section>
--- sec29-React-patterns-and-best-practices/src/components/SearchableList/SearchableList.jsx
+++ sec29-React-patterns-and-best-practices/src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
-export default function SearchableList({items, children}) {
+export default function SearchableList({items, itemKeyFn, children}) {
+    const lastChange = useRef();
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter((item) => 
@@ -8,15 +9,22 @@ export default function SearchableList({items, children}) {
     )
 
     function handleChange(event) {
-        setSearchTerm(event.target.value)
+        if (lastChange.current) {
+            clearTimeout(lastChange.current)
+        }
+
+        lastChange.current = setTimeout(() => {  // debouncing - so that state is updated only when the user stops typing for a while in the input field and then the search results are filtered. otherwise, if we update state and filter search results on every keystroke, that's inefficient
+            lastChange.current = null
+            setSearchTerm(event.target.value)
+        }, 500)
     }
 
     return (
         <div className="searchable-list">
             <input type='search' placeholder="Search" onChange={handleChange} />
             <ul>
-                {searchResults.map((item, index) => (
-                    <li key={index}>{children(item)}</li>  // calling the children func
+                {searchResults.map((item) => (  // itemKeyFn dynamically generates a specific key for specific items. using index as key aint a good idea
+                    <li key={itemKeyFn(item)}>{children(item)}</li>  // calling the children func
                 ))}
             </ul>
         </div>
