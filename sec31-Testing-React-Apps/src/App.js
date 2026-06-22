aa54d3144784a034e72220f324642b1295942798
bug2007 <moumitah2007@gmail.com>
2026-06-22 16:54:09 +0600

    wrote first test

--- sec31-Testing-React-Apps/src/App.js
+++ sec31-Testing-React-Apps/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,10 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* checking for this in the App.test.js file */}
           Learn React   
         </a>
-      </header>
+      </header> */}
+      <Greeting />
     </div>
   );
 }
--- sec31-Testing-React-Apps/src/App.test.js
+++ sec31-Testing-React-Apps/src/App.test.js
@@ -2,9 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // 'npm test' to run the test
-test('renders learn react link', () => { // 1st argument: desc. of the test (upto us). 2nd arugment: contains the actual testing code
-  render(<App />);  // App is rendered on a virtual screen (simulated browser)
-  const linkElement = screen.getByText(/learn react/i);  // grabs an element from the virtual screen. identifying the element by the text that is rendered inside of it in a case insensitive way. it's a regular exp.
-  expect(linkElement).toBeInTheDocument();  // check if the element is actually in the actual document
-});
+
+// test('renders learn react link', () => { // 1st argument: desc. of the test (upto us). 2nd arugment: contains the actual testing code
+//   render(<App />);  // App is rendered on a virtual screen (simulated browser)
+//   const linkElement = screen.getByText(/learn react/i);  // grabs an element from the virtual screen. identifying the element by the text that is rendered inside of it in a case insensitive way. it's a regular exp.
+//   expect(linkElement).toBeInTheDocument();  // check if the element is actually in the actual document
+// });
  
