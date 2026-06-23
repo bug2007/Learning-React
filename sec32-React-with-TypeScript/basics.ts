bb4f6e3df23078538ca143f2b2a6098c3d27e76b
bug2007 <moumitah2007@gmail.com>
2026-06-23 07:03:31 +0600

    worked with array and object types

--- sec32-React-with-TypeScript/basics.ts
+++ sec32-React-with-TypeScript/basics.ts
@@ -11,3 +11,21 @@ userName = 'Max';
 
 let isInstructor: boolean = false;
 
+// More complex types
+let hobbies: string[];  // wanna have an arr of strings
+hobbies = ['Sports', 'Cooking']
+
+let person: {
+    name: string,  // defining what type of values the object keys should have and what object keys shud be in the object
+    age: number
+}
+
+person = {
+    name: 'Max',
+    age: 12
+}
+
+let people: {
+    name: string,
+    age: number
+}[];  // wanna have an arr of such objects
