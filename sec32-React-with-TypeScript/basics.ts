b96be54aeee66fa77a30e36b900b7c33c5971255
bug2007 <moumitah2007@gmail.com>
2026-06-23 06:50:25 +0600

    explore primitive types

---
+++ sec32-React-with-TypeScript/basics.ts
@@ -0,0 +1,13 @@
+// Primitives: number, string, boolean, null, undefined, symbols
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 12.1; // cud assign both integer and float
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean = false;
+
